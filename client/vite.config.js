df51bf7b64b4200d191a97e1907e4c365ff6d3d2
Oscar Lin <ol78@cs431s25-5.cs.rutgers.edu>
2025-02-25 22:16:03 +0000

    change host to work not only with localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+      host:process.env.HOST,
+      port:process.env.PORT,
+  },
+  optimizeDeps:{
+      include: ["react-router-dom"]
+  }
 })
